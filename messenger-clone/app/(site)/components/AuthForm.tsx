278cda79930d75157fa2834fa66b22e58cff84a8
Sa-Te <saishark27@gmail.com>
2023-05-20 07:40:36 +0530

    Added Register and Login functionality and Authentication using NextAuth

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -4,6 +4,11 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -35,11 +40,27 @@ const AuthForm = () => {
     setLoading(true);
 
     if (variant == "REGISTER") {
-      //axios register
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong"))
+        .finally(() => setLoading(false));
     }
 
     if (variant == "LOGIN") {
-      //next auth signin
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid Credentials");
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in");
+          }
+        })
+        .finally(() => setLoading(false));
     }
   };
 
@@ -54,7 +75,13 @@ const AuthForm = () => {
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant == "REGISTER" && (
-            <Input label="name" register={register} id="name" errors={errors} />
+            <Input
+              label="Name"
+              register={register}
+              id="name"
+              errors={errors}
+              disabled={loading}
+            />
           )}
           <Input
             label="Email Address"
@@ -62,6 +89,7 @@ const AuthForm = () => {
             type="email"
             register={register}
             errors={errors}
+            disabled={loading}
           />
           <Input
             label="Password"
@@ -69,6 +97,7 @@ const AuthForm = () => {
             type="password"
             register={register}
             errors={errors}
+            disabled={loading}
           />
           <div>
             <Button disabled={loading} fullWidth type="submit">
@@ -76,6 +105,39 @@ const AuthForm = () => {
             </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-3" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {variant == "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant == "LOGIN" ? "Create an Account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
